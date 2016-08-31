21da91fd6d1d86a73a676c01dd145a0bb4f60428
Kirby D Trump <kirby.trump@software.dell.com>
2016-08-31 17:16:35 -0400

    initial

---
+++ automation/03-Provision-Microservices.ps1
@@ -0,0 +1,41 @@
+﻿#[CmdletBinding()]
+#Param(
+#    [Parameter(Mandatory=$True, Position=0, HelpMessage="The name of the Azure Subscription for which you've imported a *.publishingsettings file.")]
+#    [string]$Subscription,
+#    [Parameter(Mandatory=$True, Position=1, HelpMessage="The name of the resource group.")]
+#    [string]$ResourceGroup,
+#    [Parameter(Mandatory=$True, Position=2, HelpMessage="The name of the Azure Region/Location: East US, Central US, West US.")]
+#    [string]$AzureLocation,
+#    [Parameter(Mandatory=$True, Position=3, HelpMessage="The common prefix for resources naming: looksfamiliar")]
+#    [string]$Prefix,
+#    [Parameter(Mandatory=$True, Position=4, HelpMessage="The suffix which is one of 'dev', 'test' or 'prod'")]
+#    [string]$Suffix,
+#    [Parameter(Mandatory=$True, Position=5, HelpMessage="debug or release.")]
+#    [string]$Configuration
+#)
+
+$Subscription = "95ec29e1-405b-4df4-8d1a-8309f371f550"
+$ResourceGroup = "kt-resource-group"
+$AzureLocation ="East US"
+$Prefix = "kt"
+$Suffix = "dev"
+$Configuration = "release"
+
+$Path = Split-Path -parent $PSCommandPath
+$path = Split-Path -parent $path
+
+Write-Verbose "Start building models automation Build-Models.ps1" -Verbose
+$command = $Path + "\models\automation\Build-Models.ps1"
+&$command -Path $Path -configuration $Configuration
+
+Write-Verbose "Start building  microservices common automation Build-Common.ps1" -Verbose
+$command = $Path + "\microservices\common\automation\Build-Common.ps1"
+&$command -Path $Path -configuration $Configuration
+
+Write-Verbose "Start building \microservices\provision\automation\01-Build-ProvisionM.ps1" -Verbose
+$command = $Path + "\microservices\provision\automation\01-Build-ProvisionM.ps1"
+&$command -Path $Path -configuration $Configuration
+
+Write-Verbose "Provision to cloud \microservices\provision\automation\02-Deploy-ProvisionM.ps1" -Verbose
+$command = $Path + "\microservices\provision\automation\02-Deploy-ProvisionM.ps1"
+&$command -Path $Path -Subscription $Subscription -ResourceGroup $ResourceGroup -AzureLocation $AzureLocation -Prefix $Prefix -Suffix $Suffix -DeployData:$true
---
+++ automation/common/Invoke-NuGet.psm1
@@ -0,0 +1,50 @@
+﻿function Invoke-NuGet { param ($assembly, $projectpath, $repo, $command)
+
+    $nugetLocation = $repo + "\Automation\tools"
+
+    #$source = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    #$Filename = [System.IO.Path]::GetFileName($source)
+    #$nuget = "$nugetLocation\$Filename"
+    $nuget = "$nugetLocation\nuget.exe"
+
+    #$wc = New-Object System.Net.WebClient
+    #$wc.DownloadFile($source, $dest)
+
+    if ($command -eq "restoreProjectJson")
+    {
+        $projJson = $projectpath + "\" + $assembly + "\" + "project.json"
+        $nugetparams = "restore", $projJson, "-SolutionDirectory", $projectpath
+        Write-Output "Call NuGet restoreProjectJson '$nugetparams'"
+        & $nuget $nugetparams
+    }
+
+    if ($command -eq "restore")
+    {
+       $packagesConfig = $projectpath + "\" + $assembly + ".sln"
+        $repositoryPath = $repo + "\nugets"
+        $nugetparams = "update", $packagesConfig, "-RepositoryPath", $repositoryPath, "-Verbose"
+        Write-Output "Call NuGet Update '$nugetparams'"
+        & $nuget $nugetparams 
+
+        $proj = $projectpath + "\"  + $assembly + ".sln"
+        $nugetparams = "restore", $proj, "-SolutionDirectory", $projectpath
+        Write-Output "Call NuGet Restore '$nugetparams'"
+        & $nuget $nugetparams
+
+     }
+    
+    if ($command -eq "pack")
+    {
+        
+        $proj = $projectpath + "\" + $assembly + "\" + $assembly + ".csproj"
+        $nugetparams = "spec", "-f", $proj  
+        Write-Output "Call NuGet Spec '$nugetparams'"
+        & $nuget $nugetparams
+
+        $nugetparams = "pack", $proj  
+        Write-Output "Call NuGet Pack '$nugetparams'"
+        & $nuget $nugetparams
+    } 
+}
+
+Export-ModuleMember -Function Invoke-NuGet
